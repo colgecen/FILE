0e0b8e31a50f2b79be0b83436772cc53aef217a0
colgecen <alperencolgecen@gmail.com>
2026-08-19 13:47:32 +0300

    chore: vite react-ts iskeletini kur

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+if (!container) {
+  throw new Error('Kok bulunamadi: #root');
+}
+
+createRoot(container).render(
+  <StrictMode>
+    <p>iskelet</p>
+  </StrictMode>,
+);
