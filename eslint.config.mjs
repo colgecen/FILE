79a39b372c986b096f51a5c964676d41e40225a0
colgecen <alperencolgecen@gmail.com>
2026-08-19 19:03:53 +0300

    chore: lint ve bicimlendirme araclarini kur

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['out/**', 'dist/**', 'node_modules/**'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/consistent-type-imports': ['error', { fixStyle: 'inline-type-imports' }],
+    },
+  },
+  eslintConfigPrettier,
+);
